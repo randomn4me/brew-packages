d5372befe0da213083215ad477476124d7b984da
Philipp Kühn <git@audacis.net>
2025-01-05 20:22:31 +0100

    feat: update to include console mode

--- dictcc.rb
+++ dictcc.rb
@@ -1,9 +1,9 @@
 class Dictcc < Formula
   desc "A Python script for installing with dict.cc"
   homepage "https://github.com/randomn4me/dictcc"
-  url "https://github.com/randomn4me/dictcc/archive/refs/tags/1.2.zip"
-  version "1.2"
-  sha256 "f3b455b3d80ea264588732ca810051cf497228525e4e58b54626ec59b7c0fe28"
+  url "https://github.com/randomn4me/dictcc/archive/refs/tags/1.3.zip"
+  version "1.3"
+  sha256 "b1de0cb15500390558804e0cadf5aa5710d946461ff16a8c5ff13f7793011363"
 
   depends_on "python@3.10" # Ensure Python 3.10 or later is available
 
