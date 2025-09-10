674d84c9882d46cb2fc350419cc026f64c28a71f
Philipp Kühn <git@audacis.net>
2025-09-10 21:33:07 +0200

    fix: added product to the build

--- Formula/mac-mpd-controls.rb
+++ Formula/mac-mpd-controls.rb
@@ -1,8 +1,8 @@
 class MacMpdControls < Formula
   desc "Lightweight command-line MPD client for macOS with media key integration"
   homepage "https://github.com/randomn4me/mac-mpd-controls"
-  url "https://github.com/randomn4me/mac-mpd-controls/archive/refs/tags/v0.1.tar.gz"
-  sha256 "2256fe916eeeb995a2bfaf8f5443c0eb8aabf016d41c9d1c99b0adecc40f1a80"
+  url "https://github.com/randomn4me/mac-mpd-controls/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "f6fa88d5435e8ba53b490570e532b6ee86e3f36d8a501b71ffb40af9706d430b"
   license "MIT"
   head "https://github.com/randomn4me/mac-mpd-controls.git", branch: "main"
 
@@ -10,13 +10,14 @@ class MacMpdControls < Formula
   depends_on :macos
 
   def install
-    system "swift", "build", "--configuration", "release", "--disable-sandbox"
-    bin.install ".build/release/mac-mpd-controls"
+    system "swift", "build", "--configuration", "release", "--disable-sandbox", "--product", "MPDControls"
+    bin.install ".build/release/MPDControls" => "mac-mpd-controls"
   end
 
   service do
     run [opt_bin/"mac-mpd-controls"]
     keep_alive true
+    session_type :aqua
     environment_variables PATH: "#{HOMEBREW_PREFIX}/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
     log_path var/"log/mac-mpd-controls.log"
     error_log_path var/"log/mac-mpd-controls.error.log"
